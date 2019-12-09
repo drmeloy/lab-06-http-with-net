2a854c310f908b805dd9d7684687b0dfbdef677b
drmeloy <drmeloy@gmail.com>
2019-12-09 15:25:39 -0700

    echo route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,21 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
+describe('app routes', () => {
+  it('handles a GET / route', () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.text).toEqual('hi');
+      });
+  });
 
+  it('handles a POST /echo route', () => {
+    return request(app)
+      .post('/echo')
+      .send('k whatever')
+      .then(response => {
+        expect(response.text).toEqual('k whatever');
+      });
+  });
 });
